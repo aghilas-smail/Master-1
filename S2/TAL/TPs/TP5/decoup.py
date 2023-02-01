50bdc79e097bb90081fc241a1f34180818b86d1e
like a Boss <aghilas.smail@gmail.com>
2023-02-01 18:10:11 +0100

    Q1 done

--- S2/TAL/TPs/TP5/decoup.py
+++ S2/TAL/TPs/TP5/decoup.py
@@ -1,20 +1,22 @@
-with open("corpus_en_200K.train.txt") as f:
-    content = f.readlines()
+lines = open('corpus_en_200k.train.txt', "r")
 
-size = len(content)
+#with open("corpus_en_200K.train.txt") as f:
+#    content = f.readlines()
+line = lines.readlines()
+size = len(line)
 taille_train = round(size*0.7)
 taille_dev = round(size*0.2)
 taille_test = round(size*0.1)
 
 f1 = open("corpus.data", "w")
 f2 = open("corpus.dev", "w")
-# f3 = open("corpus.test", "w")
+f3 = open("corpus.test", "w")
 
 for i in range(size):
-    line = content[i]
+    l = line[i]
     if i < taille_train:
-        f1.write(line)
-    else:
-        f2.write(line)
-    # else:
-    #     f3.write(line)
+        f1.write(l)
+    if i < taille_dev:
+        f2.write(l)
+    if i < taille_test:
+        f3.write(l)
