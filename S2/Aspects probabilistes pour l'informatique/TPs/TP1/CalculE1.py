2b71a1ef7d81e6a7f6cf9a632fed0e6e0a43cff3
like a Boss <aghilas.smail@gmail.com>
2023-03-20 10:27:34 +0100

    Tp1 Exo 2 Proba first pas

---
+++ S2/Aspects probabilistes pour l'informatique/TPs/TP1/CalculE1.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+somme = 0
+n = 0
+
+while somme <= 1:
+    r= random.random()
+    somme +=r
+    n = n+1
+print(n)
+
+'''
+
+Pour la question 2:
+Pour oubtenir une experience  a 3 il faut réaliser au moin 2 experience '''
